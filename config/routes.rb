1a5ed525cbbc5ce5fb5d0b952acd478946535a28
Yan Tadeu <yan.tadeu1@gmail.com>
2018-10-31 16:35:37 -0300

    tickers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'scrap', action: :create, controller: 'scrapper'
       get 'search', action: :search, controller: 'scrapper'
+      get 'tickers', action: :tickers, controller: 'scrapper'
     end
   end
 
